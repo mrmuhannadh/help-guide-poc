1ca941b5069fb8cb24e67a242a8583465fd7ba32
muhannadhr <muhannadh.razick@directfn.com>
2025-11-26 16:13:52 +0530

    content updates

--- components/figure/figure-element.tsx
+++ components/figure/figure-element.tsx
@@ -16,11 +16,15 @@ type Props = {
 export default function ThemedLocaleFigure({ baseName, alt, description = '', ...props }: Props) {
   const { increment } = useFigureCounter();
   const [figureNumber, setFigureNumber] = useState(0);
+  const [captionEnabled, setCaptionEnabled] = useState(false);
 
   useEffect(() => {
-    setFigureNumber(increment(alt));
+    if (description.trim() !== '') {
+      setCaptionEnabled(true);
+      setFigureNumber(increment(alt));
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [description]);
 
   return (
     <figure className='my-8'>
@@ -29,9 +33,11 @@ export default function ThemedLocaleFigure({ baseName, alt, description = '', ..
         alt={alt}
         {...props}
       />
-      <figcaption className='text-md mt-2 text-gray-500 dark:text-neutral-400'>
+      {captionEnabled && (
+        <figcaption className='text-md mt-2 text-gray-500 dark:text-neutral-400'>
         <span className='font-bold'>Figure {figureNumber}:</span> {description}
       </figcaption>
+      )}
     </figure>
   );
 }
